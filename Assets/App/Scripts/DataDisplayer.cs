7e7177b89b6d32cdf951a9fab2fc9f25a7bb111f
Joost van Schaik <joostvanschaik@outlook.com>
2018-11-01 20:38:22 +0100

    Added interaction

---
+++ Assets/App/Scripts/DataDisplayer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+public class DataDisplayer : MonoBehaviour, IInputClickHandler, IFocusable
+{
+    private Vector3 _originalScale;
+
+    private Dictionary<MeshRenderer, Color[]> _originalColors = new Dictionary<MeshRenderer, Color[]>();
+
+    void Start()
+    {
+        SaveOriginalColors();
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        Debug.Log("Click " + gameObject.name);
+    }
+
+    public void OnFocusEnter()
+    {
+        SetHighlight(true);
+    }
+
+    public void OnFocusExit()
+    {
+        SetHighlight(false);
+    }
+
+    private void SaveOriginalColors()
+    {
+        if (!_originalColors.Any())
+        {
+            foreach (var component in GetComponentsInChildren<MeshRenderer>())
+            {
+                var colorList = new List<Color>();
+
+                foreach (var t in component.materials)
+                {
+                    colorList.Add(t.color);
+                }
+                _originalColors.Add(component, colorList.ToArray());
+            }
+        }
+    }
+
+    private void SetHighlight(bool status)
+    {
+        var targetColor = Color.red;
+        foreach (var component in GetComponentsInChildren<MeshRenderer>())
+        {
+            for (var i = 0; i < component.materials.Length; i++)
+            {
+                component.materials[i].color = status ? targetColor : _originalColors[component][i];
+            }
+        }
+    }
+}
---
+++ Assets/App/Scripts/InteractionBuilder.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class InteractionBuilder : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    foreach (var child in GetComponentsInChildren<MeshFilter>())
+	    {
+	        child.gameObject.AddComponent<MeshCollider>();
+	        child.gameObject.AddComponent<DataDisplayer>();
+	    }
+	}
+}
